aabd828c374c66de15d5ceed22c81c3d77df6c37
alannahof <33405594+alannahof@users.noreply.github.com>
2018-05-21 10:25:33 +1200

    first commit

--- app/src/main/java/com/example/filealan/youniverse/MainActivity.java
+++ app/src/main/java/com/example/filealan/youniverse/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.filealan.youniverse;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+TextView clickText;
+TextView userName;
+TextView password;
+TextView welcomeText;
+EditText userNameField;
+EditText passwordField;
+Button proceedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickText = (TextView)findViewById(R.id.clickText);
+        userName = (TextView)findViewById(R.id.userName);
+        password = (TextView)findViewById(R.id.password);
+        welcomeText = (TextView)findViewById(R.id.welcomeText);
+        userNameField = (EditText)findViewById(R.id.userNameField);
+        passwordField = (EditText)findViewById(R.id.passwordField);
+        proceedButton = (Button)findViewById(R.id.proceedButton);
+
+        clickText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.sign_up);
+            }
+        });
+
     }
 }
+
+
+
+
